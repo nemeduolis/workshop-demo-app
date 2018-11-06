27552c32a63fdbdf16327df89cb02f0d00bcbbe0
nemeduolis <31727400+nemeduolis@users.noreply.github.com>
2018-11-06 17:59:44 +0200

    Update unit_test.py

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,16 +1,15 @@
 from lib.demo import add, subtract, multiply
 
-
 def test_add():
     result = add(2, 2)
     assert result == 4
 
 
 def test_subtract():
-     result = subtract(4, 2)
-     assert result == 2
+    result = subtract(4, 2)
+    assert result == 2
 
 
- def test_multiply():
+def test_multiply():
     result = multiply(3, 2)
     assert result == 6
